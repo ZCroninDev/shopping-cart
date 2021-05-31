454480dc0748f75f9aefa6bb92cb8b3645e1e4ed
zach <ZCroninDev@users.noreply.github.com>
2021-05-31 11:39:23 -0400

    Adds Remove button and cart total logic

--- store.js
+++ store.js
@@ -1,3 +1,7 @@
+if (DocumentFragment.readtState == 'laoding') {
+    document.addEventListener('DOMContentLoaded')
+}
+
 var removeCartItemButtons = document.getElementsByClassName('btn-danger')
 console.log(removeCartItemButtons)
 for (var i = 0; i < removeCartItemButtons.length; i++) {
@@ -12,11 +16,15 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
 function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')
+        var quanityElement = cartRow.getElementsByClassName('cart-quantity-input')
         [0]
-        console.log(priceElement, quantityElement)
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quanity = quanityElement.value
+        total = total + (price * quanity)
     }
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
